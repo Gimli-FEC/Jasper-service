f9111d2d613baeba583c2863cde5e366dcf72ad7
JasperC1999 <jasperischauvin@gmail.com>
2020-05-01 18:00:58 -0500

    added api routes && db queries

--- database/index.js
+++ database/index.js
@@ -13,7 +13,39 @@ connection.connect((err) => {
   console.log('Connected!');
 });
 
+const getDetails = (id, callback) => {
+  connection.query(`SELECT details FROM games where id="${id}"`, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, results);
+    }
+  })
+}
+
+const getScreenshots = (id, callback) => {
+  connection.query(`SELECT link FROM screenshots WHERE game_id="${id}";`, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, results);
+    }
+  })
+}
+
+const getVideos = (id, callback) => {
+  connection.query(`SELECT link FROM videos WHERE game_id="${id}";`, (err, results) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, results);
+    }
+  })
+}
 
 module.exports = {
-  connection
+  connection,
+  getDetails,
+  getScreenshots,
+  getVideos
 }
