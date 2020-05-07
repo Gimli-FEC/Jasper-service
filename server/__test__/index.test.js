791ede0b31c0a5e5e1969d9f51036e5cc7232821
JasperC1999 <jasperischauvin@gmail.com>
2020-05-07 10:41:20 -0500

    added second JSON test to server side

--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -1,15 +1,24 @@
 const app = require('../index');
 const supertest = require('supertest');
-const details = require('./details.json');
+const details30 = require('./details30.json');
+const details75 = require('./details75.json');
 
 const request = supertest(app);
 
 app.listen(3002, () => console.log(`listening on port 3002!!!!!!!!!!!`));
 
 it('gets the games endpoint at 30', async done => {
-  const response = await request.get('/games/50')
+  const response30 = await request.get('/games/30')
+  console.log(response30.body)
+  expect(response30.status).toBe(200);
+  expect(response30.body).toMatchObject(details30);
+  done();
+})
 
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchObject(details);
+it('gets the games endpoint at 75', async done => {
+  const response75 = await request.get('/games/75')
+
+  expect(response75.status).toBe(200);
+  expect(response75.body).toMatchObject(details75);
   done();
 })
